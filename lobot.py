ff4819d1bdbaeceec962814b05ddb9d7d3bc8a92
Merlin Sch?ler <merlin.schueler@ini.rub.de>
2019-01-21 11:51:38 +0100

    Added high-price CPU type 'c5.18xlarge' to recommended types

--- lobot.py
+++ lobot.py
@@ -13,12 +13,13 @@ from shutil import get_terminal_size
 STANDARD_ATTRIBUTES = ["Name", "KeyName", "InstanceId", "InstanceType", "PublicIpAddress", "Uptime", "State", "AvailabilityZone"]
 RECOMMENDED_INSTANCE_TYPES = {"t3.nano":"Very cheap general purpose instance, good for testing workflows.",
         "t3.large":"Cheap general purpose instance, good for smaller tasks, e.g. mild preprocessing.",
-        "c5.2xlarge":"Medium price CPU instance, good for not-massively parallel tasks, e.g. small keras models, sklearn machine learning.",
+        "c5.2xlarge":"Medium price CPU instance (vCPU=8), good for not-massively parallel tasks, e.g. small keras models, sklearn machine learning.",
         "g3s.xlarge":"Medium price nVidia (M60)  instance, good for almost all GPU tasks.",
         "p2.xlarge":"Medium price nVidia (K80) instance, useful if g3s.xlarge is already taken.",
         "r5.2xlarge":"Medium price RAM instance, good for memory-demanding tasks, e.g. preprocessing, expansion/reduction workflows. Has 64GB RAM.",
         "p3.2xlarge":"High price nVidia (V100) instance, good for demanding GPU tasks.",
         "r5.12xlarge":"High price RAM instance, good for extremely memory-demanding tasks. Has 384GB RAM."}
+        "c5.18xlarge":"High price CPU instance (vCPU=72), good for highly parallel non-GPU tasks, e.g. hyperparameter-tuning on CPU models.",
 
 
 def check_port(port):
